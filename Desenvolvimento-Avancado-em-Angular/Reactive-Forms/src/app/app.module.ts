2a575c324692a72614ea3321f9a3c26bcc3af455
Danilo <danilo.silva@msn.com>
2023-07-08 12:56:28 -0300

    Criação do Projeto + Template

--- Desenvolvimento-Avancado-em-Angular/Reactive-Forms/src/app/app.module.ts
+++ Desenvolvimento-Avancado-em-Angular/Reactive-Forms/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { RodapeComponent } from './navegacao/rodape/rodape.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    HomeComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
